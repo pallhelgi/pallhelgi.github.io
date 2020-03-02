ffd9aa06b50147b024bf239707584dc589de3c98
pallhelgi <pallhelgi@gmail.com>
2020-03-02 17:48:51 +0000

    deploy

---
+++ precache-manifest.56222fc0939b1b8c4dcab274c280c403.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5600a8c9a8237fdba74285102dbf8fcf",
+    "url": "/PHSLOGO.png"
+  },
+  {
+    "revision": "40bade876fb85b80cb2c",
+    "url": "/css/app.27f9a98c.css"
+  },
+  {
+    "revision": "23ec4b15a06909b834474f86301deccf",
+    "url": "/img/codecat.23ec4b15.png"
+  },
+  {
+    "revision": "f122bcd65d6b975749e1eff9515f0cd0",
+    "url": "/img/golf.f122bcd6.png"
+  },
+  {
+    "revision": "bf9dbfd09f39f558b712acbaa38c5efb",
+    "url": "/img/pall_helgi_sigurdarson_svhv_1.bf9dbfd0.jpg"
+  },
+  {
+    "revision": "52fc5d7ce98f61b4061839ab61eee282",
+    "url": "/img/payadvice.52fc5d7c.png"
+  },
+  {
+    "revision": "bafcf87dbf501bb1a45d679217cc9500",
+    "url": "/img/scrumdiagram.bafcf87d.png"
+  },
+  {
+    "revision": "c2b65964414a2e6b43609963ec3b346a",
+    "url": "/img/supergroup.c2b65964.png"
+  },
+  {
+    "revision": "b24db37e907709995e32e1eecec0e137",
+    "url": "/index.html"
+  },
+  {
+    "revision": "40bade876fb85b80cb2c",
+    "url": "/js/app.90a8c834.js"
+  },
+  {
+    "revision": "49e8d4c16ab0bc37b00a",
+    "url": "/js/chunk-vendors.abd2a066.js"
+  },
+  {
+    "revision": "bf642f7653ab44448e91f3e434fb6f1c",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
